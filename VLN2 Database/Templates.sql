c73c73f6caffdc5f1647665219feb9a3064d176d
Guðmundur Axel Guðrúnarson <brucejackie21@gmail.com>
2017-04-30 20:43:08 +0000

    Created Templates table.

---
+++ VLN2 Database/Templates.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Templates]
+(
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(MAX) NOT NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+    [Code] NVARCHAR(MAX) NOT NULL
+)
