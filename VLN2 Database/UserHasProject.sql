57202fd715abec64e733b9023cc0251e58971d19
Guðmundur Axel Guðrúnarson <brucejackie21@gmail.com>
2017-04-30 21:21:58 +0000

    Created UserHasProject table.

---
+++ VLN2 Database/UserHasProject.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserHasProject]
+(
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserID] INT NOT NULL, 
+    [ProjectID] INT NOT NULL, 
+    [ProjectRoleID] INT NOT NULL, 
+    CONSTRAINT [FK_UserHasProject_Projects] FOREIGN KEY ([ProjectID]) REFERENCES [Projects]([ID]) 
+)
