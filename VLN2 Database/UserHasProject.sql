78cc90726b38a8cc4bfc5c01e2ca415aa69d3ae2
Guðmundur Axel Guðrúnarson <brucejackie21@gmail.com>
2017-04-30 21:47:36 +0000

    Set up foreign keys on UserHasProject table.

--- VLN2 Database/UserHasProject.sql
+++ VLN2 Database/UserHasProject.sql
@@ -4,5 +4,7 @@
     [UserID] INT NOT NULL, 
     [ProjectID] INT NOT NULL, 
     [ProjectRoleID] INT NOT NULL, 
-    CONSTRAINT [FK_UserHasProject_Projects] FOREIGN KEY ([ProjectID]) REFERENCES [Projects]([ID]) 
+    CONSTRAINT [FK_UserHasProject_Projects] FOREIGN KEY ([ProjectID]) REFERENCES [Projects]([ID]), 
+    CONSTRAINT [FK_UserHasProject_ProjectRoles] FOREIGN KEY ([ProjectRoleID]) REFERENCES [ProjectRoles]([ID]), 
+    CONSTRAINT [FK_UserHasProject_Users] FOREIGN KEY ([UserID]) REFERENCES [Users]([ID]) 
 )
