b282d1c3a34be270d3b304bbb72f3d0be9a4f32d
Guðmundur Axel Guðrúnarson <brucejackie21@gmail.com>
2017-04-30 21:07:45 +0000

    Added foreign keys to ConversationHasMessage table.

--- VLN2 Database/ConversationHasMessage.sql
+++ VLN2 Database/ConversationHasMessage.sql
@@ -2,5 +2,7 @@
 (
 	[ID] INT NOT NULL PRIMARY KEY IDENTITY, 
     [ConversationID] INT NOT NULL, 
-    [MessageID] INT NOT NULL
+    [MessageID] INT NOT NULL, 
+    CONSTRAINT [FK_ConversationHasMessage_Conversations] FOREIGN KEY ([ConversationID]) REFERENCES [Conversations]([ID]), 
+    CONSTRAINT [FK_ConversationHasMessage_Messages] FOREIGN KEY ([MessageID]) REFERENCES [Messages]([ID])
 )
