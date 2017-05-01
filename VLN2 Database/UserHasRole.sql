0fabd6518cb3dea0f2f590f887a22d5c32dce1b1
Guðmundur Axel Guðrúnarson <brucejackie21@gmail.com>
2017-05-01 11:51:47 +0000

    Fixed table collision with UserRoles.

---
+++ VLN2 Database/UserHasRole.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[UserHasRole] (
+    [UserID] INT NOT NULL,
+    [RoleID] INT NOT NULL,
+    CONSTRAINT [PK_dbo.UserHasRole] PRIMARY KEY CLUSTERED ([UserID] ASC, [RoleID] ASC),
+    CONSTRAINT [FK_dbo.UserHasRole_dbo.Roles_RoleID] FOREIGN KEY ([RoleID]) REFERENCES [dbo].[Roles] ([ID]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.UserHasRole_dbo.Users_UserID] FOREIGN KEY ([UserID]) REFERENCES [dbo].[Users] ([ID]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_UserID]
+    ON [dbo].[UserHasRole]([UserID] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_RoleID]
+    ON [dbo].[UserHasRole]([RoleID] ASC);
+
+
--- VLN2 Database/UserRoles.sql
+++ VLN2 Database/UserRoles.sql
@@ -1,18 +1,6 @@
-﻿CREATE TABLE [dbo].[UserRoles] (
-    [UserID] INT NOT NULL,
-    [RoleID] INT NOT NULL,
-    CONSTRAINT [PK_dbo.UserRoles] PRIMARY KEY CLUSTERED ([UserID] ASC, [RoleID] ASC),
-    CONSTRAINT [FK_dbo.UserRoles_dbo.Roles_RoleID] FOREIGN KEY ([RoleID]) REFERENCES [dbo].[Roles] ([ID]) ON DELETE CASCADE,
-    CONSTRAINT [FK_dbo.UserRoles_dbo.Users_UserID] FOREIGN KEY ([UserID]) REFERENCES [dbo].[Users] ([ID]) ON DELETE CASCADE
-);
-
-
-GO
-CREATE NONCLUSTERED INDEX [IX_UserID]
-    ON [dbo].[UserRoles]([UserID] ASC);
-
-
-GO
-CREATE NONCLUSTERED INDEX [IX_RoleID]
-    ON [dbo].[UserRoles]([RoleID] ASC);
-
+﻿Create TABLE [dbo].[UserRoles]
+(
+	[ID] int NOT NULL PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Description] NVARCHAR(MAX) NULL
+)
