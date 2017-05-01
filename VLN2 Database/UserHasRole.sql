51d3a04018733156b6809ce4b61405bf9cd6e675
Guðmundur Axel Guðrúnarson <brucejackie21@gmail.com>
2017-05-01 11:59:39 +0000

    Removed duplicate Roles table.

--- VLN2 Database/UserHasRole.sql
+++ VLN2 Database/UserHasRole.sql
@@ -2,7 +2,7 @@
     [UserID] INT NOT NULL,
     [RoleID] INT NOT NULL,
     CONSTRAINT [PK_dbo.UserHasRole] PRIMARY KEY CLUSTERED ([UserID] ASC, [RoleID] ASC),
-    CONSTRAINT [FK_dbo.UserHasRole_dbo.Roles_RoleID] FOREIGN KEY ([RoleID]) REFERENCES [dbo].[Roles] ([ID]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.UserHasRole_dbo.Roles_RoleID] FOREIGN KEY ([RoleID]) REFERENCES [dbo].[UserRoles] ([ID]) ON DELETE CASCADE,
     CONSTRAINT [FK_dbo.UserHasRole_dbo.Users_UserID] FOREIGN KEY ([UserID]) REFERENCES [dbo].[Users] ([ID]) ON DELETE CASCADE
 );
 
--- VLN2 Database/UserRoles.sql
+++ VLN2 Database/UserRoles.sql
@@ -1,6 +1,11 @@
-﻿Create TABLE [dbo].[UserRoles]
-(
-	[ID] int NOT NULL PRIMARY KEY IDENTITY,
-	[Name] NVARCHAR(50) NOT NULL,
-	[Description] NVARCHAR(MAX) NULL
-)
+﻿CREATE TABLE [dbo].[UserRoles] (
+    [ID]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (256) NOT NULL,
+    [Description] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [PK_dbo.UserRoles] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [RoleNameIndex]
+ON [dbo].[UserRoles]([Name] ASC);
