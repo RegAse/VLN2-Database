be367491941946b47cff8a54fbf212582c7c04ab
Guðmundur Axel Guðrúnarson <brucejackie21@gmail.com>
2017-05-04 13:30:21 +0000

    Added ProjectFiles table.

---
+++ VLN2 Database/ProjectFiles.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ProjectFiles]
+(
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(256) NOT NULL, 
+    [Content] NVARCHAR(MAX) NULL, 
+    [ProjectID] INT NOT NULL, 
+    [IsFolder] BIT NOT NULL, 
+    [ProjectFileID] INT NULL, 
+    CONSTRAINT [FK_ProjectFiles_Projects] FOREIGN KEY ([ProjectID]) REFERENCES [Projects]([ID])
+)
