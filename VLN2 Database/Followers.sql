23efa9dc9e4a0f6d6d4f576fa11a30f76ed753ea
Guðmundur Axel Guðrúnarson <brucejackie21@gmail.com>
2017-05-05 23:33:08 +0000

    Added Followers table.

---
+++ VLN2 Database/Followers.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Followers]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [UserID] INT NOT NULL, 
+    [UserFollowID] INT NOT NULL, 
+    CONSTRAINT [FK_Followers_Users] FOREIGN KEY ([UserID]) REFERENCES [Users]([ID]), 
+    CONSTRAINT [FK_Follow_Users] FOREIGN KEY ([UserID]) REFERENCES [Users]([ID])
+)
